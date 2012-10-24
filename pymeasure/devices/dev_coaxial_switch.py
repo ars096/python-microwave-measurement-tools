991cb3ab11ae16f827dffc10a337668020660f94
bajihiroyuki <s_h.baji@p.s.osakafu-u.ac.jp>
2012-10-24 22:40:39 +0900

    create coaxial switch

--- pymeasure/devices/dev_coaxial_switch.py
+++ pymeasure/devices/dev_coaxial_switch.py
@@ -9,11 +9,19 @@ import device
 
 def select_coaxial_switch(command_type, *args, **kwargs):
     command_type = command_type.lower()
-    #if command_type=='scpi': return SCPI_command(*args, **kwargs)
+    if command_type=='scpi': return SCPI_command(*args, **kwargs)
     return None
 
 
 class coaxial_switch(object):
+    @device._open_close
+    def check_supply_voltage(self):
+        pass
+
+    @device._open_close
+    def set_supply_voltage(self):
+        pass
+
     @device._open_close
     def set_open_switch(self, ch):
         pass
@@ -30,3 +38,35 @@ class coaxial_switch(object):
     def check_close_switch(self, ch):
         pass
 
+
+class SCPI_command(coaxial_switch, device.scpi_device):
+    @device._open_close
+    def check_supply_voltage(self, ch=1):
+        """
+        -- Method --
+        query supply voltage
+        """
+        self.com.send('CONFigure:BANK%d?' %ch)
+        return self.com.readline()
+
+
+#    @device._open_close
+#    def set_supply_voltage(self):
+#        self.com.send('CONFigure:BANK%d P24v')
+#        return self.check_supply_voltage(ch)
+
+    @device._open_close
+    def set_open_switch(self, ch):
+        pass
+
+    @device._open_close
+    def check_open_switch(self, ch):
+        pass
+
+    @device._open_close
+    def set_close_swicth(self, ch):
+        pass
+
+    @device._open_close
+    def check_close_switch(self, ch):
+        pass
---
+++ test/test_coaxial_switch.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+
+"""
+Documents
+"""
+
+
+TARGET = ('192.168.100.210', 1234, 28)
+COMMAND_TYPE = 'SCPI'
+CONNECTION_METHOD = 'GPIB_PROLOGIX'
+
+import pymeasure
+import pymeasure.devices
+import pymeasure.communicators
+import unittest
+
+class Test_Coaxial_Switch(unittest.TestCase):
+    def test_initialize(self):
+        self.assertIsInstance(pymeasure.devices.coaxial_switch(COMMAND_TYPE),
+                              pymeasure.devices.dev_coaxial_switch.coaxial_switch)
+
+    def test_set_communicator(self):
+        com = pymeasure.create_communicator(CONNECTION_METHOD, *TARGET)
+        cs = pymeasure.devices.coaxial_switch(COMMAND_TYPE)
+        self.assertIsInstance(cs.set_communicator(com),
+                              pymeasure.communicators.communicator.communicator)
+
+    def test_check_supply_voltage(self):
+        com = pymeasure.create_communicator(CONNECTION_METHOD, *TARGET)
+        cs = pymeasure.devices.coaxial_switch(COMMAND_TYPE, com)
+        print cs.check_supply_voltage()
+
+    def test_set_supply_voltage(self):
+        pass
+
+    def test_check_open_switch(self):
+        pass
+
+    def test_set_open_switch(self):
+        pass
+
+    def test_check_close_switch(self):
+        pass
+
+    def test_set_close_switch(self):
+        pass
+
+
+
+if __name__=='__main__':
+    unittest.main()
