604d56b5bb6659a41b713d250e5268cce336824b
Atsushi NISHIMURA <ars096@gmail.com>
2012-11-28 02:14:18 +0900

    ADD docstrings: pymeasure.devices.dev_attenuator

--- pymeasure/devices/dev_attenuator.py
+++ pymeasure/devices/dev_attenuator.py
@@ -9,29 +9,84 @@ import device
 
 def select_attenuator(command_type, *args, **kwargs):
     command_type = command_type.lower()
-    #if command_type=='scpi': return SCPI_command(*args, **kwargs)
+    #if command_type=='scpi': return SCPI(*args, **kwargs)
     if command_type in ['elva1', 'elva-1']:
-        return elva1_command(*args, **kwargs)
+        return elva1(*args, **kwargs)
     return None
 
 
 class attenuator(object):
+    """
+    Attenuator
+    ==========
+
+    使用できるメソッド
+    ------------------
+    * set_level(level)
+    * check_level()
+
+    * get_product_information()
+
+    使用できるプロパティ
+    -------------------
+    * tested_products
+    """
     def set_level(self, level):
+        """
+        アッテネータのレベルを設定します。
+
+        Parameters
+        ----------
+        level : float
+            設定するレベル
+
+        Returns
+        -------
+        level : float, 設定後のレベル
+
+        Examples
+        --------
+        >>> att.set_level(20)
+        20
+        """
         pass
 
-    def check_level(self, level):
+    def check_level(self):
+        """
+        アッテネータのレベルを確認します。
+
+        Parameters
+        ----------
+        None :
+
+        Returns
+        -------
+        level : float, レベル
+
+        Examples
+        --------
+        >>> att.check_level()
+        20
+        """
         pass
 
 
-class elva1_command(attenuator, device.scpi_device):
+class elva1(attenuator, device.scpi_device):
     """
-    tested products
-    ===============
-        ELVA1
-        -----------
-            GPDVC-15/100/RS, GPIB
+    ELVA1
+    =====
+
+    使用できるメソッド
+    ------------------
+    * set_gpib_address(gpib)
 
     """
+    tested_products = {
+        'ELVA1':[
+            'GPDVC-15/100/RS, GPIB',
+            ]
+    }
+
     level = None
 
     def check_level(self):
@@ -50,8 +105,29 @@ class elva1_command(attenuator, device.scpi_device):
 
     @device._open_close
     def set_gpib_address(self, gpib):
+        """
+        GPIBアドレスを設定します。
+
+        Parameters
+        ----------
+        gpib : int
+            設定するGPIBアドレス
+
+        Returns
+        -------
+        gpib : int, 設定後のGPIBアドレス
+
+        Examples
+        --------
+        >>> att.set_gpib_address(21)
+        21
+        """
         self.com.send('SYST:COMM:GPIB:ADDR %d'%gpib)
         self.com.set_gpib_address(gpib)
         self.com.send('*SAV 0')
         return self.get_gpib_address()
 
+    # set doc strings
+    __doc__ = attenuator.__doc__
+    set_level.__doc__ = attenuator.set_level.__doc__
+    check_level.__doc__ = attenuator.check_level.__doc__
